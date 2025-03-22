a82f6838bfa4135c6f49b430fe9c158a68786e83
manuatrejo <148013542+manuatrejo@users.noreply.github.com>
2025-03-22 12:58:18 +0100

    Add files via upload

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,5 +1,55 @@
 // gestión de el here_doc
-here_doc
+
+char	*path_in_env(char *path, char **env)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (env[++i])
+	{
+		j = 0;
+		while (path[j] == env[i][j])
+			j++;
+		if (j == 4)
+			return (env[i] + 5);//porque es el siguiente a PATH=
+	}
+	return (NULL);
+}
+
+char	*execve_input(char *cmd1, char **env)
+{
+	int		i;
+	char	*temp;
+	char	*input;
+	char	**paths;
+
+	i = -1;
+	paths = ft_split(path_in_env("PATH", env), ':');
+	while (paths[++i])
+	{
+		temp = ft_strjoin(paths[i], "/");
+		input = ft_strjoin(temp, cmd1);
+		free(temp);
+		if (access(temp, X_OK | F_OK) == 0)
+		{
+			free_array(paths);
+			return (input);
+		}
+		free(input);
+	}
+	free_array(paths);
+}
+
+void	exec(char *cmd, char **env)
+{
+	char	**cmds;
+
+	cmds = ft_split(cmd, ' ');
+	if (execve(execve_input(cmds[0], env), cmds, env) == -1)
+		free_array(cmds);
+		
+}
 
 void	pipes(char **env, char *cmd)
 {
@@ -10,7 +60,7 @@ void	pipes(char **env, char *cmd)
 	pipe(pipe_fds);
 	pid = fork();
 	if (pid < 0)
-		exit;
+		exit(0);
 	if (pid != 0)
 	{
 		close(pipe_fds[0]);
